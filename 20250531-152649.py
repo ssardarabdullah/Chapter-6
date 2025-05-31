aa64121ab1fb5eb6ebb4f298b2aff513d14361c2
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 15:30:23 +0500

    Update 20250531-152649.py

--- 20250531-152649.py
+++ 20250531-152649.py
@@ -1,3 +1,24 @@
+Question6______
+so each person can have more 
+than one favorite number. 
+Then print each per￾son’s name
+along with their favorite numbers.
+
+solution __________
+
+favorite_numbers = {
+    'Alice': [7, 42],
+    'Bob': [3],
+    'Charlie': [5, 9, 14],
+    'Diana': [8, 22],
+}
+for name, numbers in favorite_numbers.items():
+    print(f"{name}'s favorite numbers are: {', '.join(str(num) for num in numbers)}")
+
+
+
+
+
 # Dictionary to store people's names and their favorite numbers
 favorite_numbers = {
     'Alice': [7, 42],
