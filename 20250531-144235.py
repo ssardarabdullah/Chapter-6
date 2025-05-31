242cdd1aafe7c573c9faa32f750fac2c1e6e93b8
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 14:46:36 +0500

    Update 20250531-144235.pychapter 6

--- 20250531-144235.py
+++ 20250531-144235.py
@@ -1,13 +1,22 @@
-# Creating a dictionary to store information about a person
+Question 1____
+Person: Use a dictionary to store
+information about a person you know.
+Store their first name, last name
+, age, and the city in which they 
+live. Youshould have keys such as 
+first_name, last_name, age, and 
+city. Print each piece of 
+information stored in your 
+dictionary.
+solution _____
+ Creating a dictionary to store information about a person
 person = {
     "first_name": "Alice",
     "last_name": "Johnson",
     "age": 28,
     "city": "New York"
 }
-
-# Printing each piece of information
-print("First Name:", person["first_name"])
-print("Last Name:", person["last_name"])
-print("Age:", person["age"])
-print("City:", person["city"])
+ print("First Name:", person["first_name"])
+ print("Last Name:", person["last_name"])
+ print("Age:", person["age"])
+ print("City:", person["city"])
