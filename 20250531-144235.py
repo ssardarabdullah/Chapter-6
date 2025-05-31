e497180a01f47a9891998977b928d07b220b8404
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 14:43:51 +0500

    Chapter5

---
+++ 20250531-144235.py
@@ -0,0 +1,13 @@
+# Creating a dictionary to store information about a person
+person = {
+    "first_name": "Alice",
+    "last_name": "Johnson",
+    "age": 28,
+    "city": "New York"
+}
+
+# Printing each piece of information
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
