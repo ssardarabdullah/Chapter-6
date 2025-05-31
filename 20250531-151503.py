93d52bad6fd90f421e9721747c5fd99392225c09
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 15:23:01 +0500

    Update 20250531-151503.pychapter6

--- 20250531-151503.py
+++ 20250531-151503.py
@@ -1,4 +1,18 @@
-# Define three dictionaries representing different people
+Question 5_____
+
+Make two new dictionaries
+representing different people,
+and store all three 
+dictionaries in a list called
+people. Loop through your list of
+people. As you 
+loop through the list, print 
+everything you know about each
+person.
+
+solution ____________
+
+
 person_1 = {
     'first_name': 'Alice',
     'last_name': 'Johnson',
@@ -20,10 +34,10 @@ person_3 = {
     'city': 'Chicago'
 }
 
-# Store all dictionaries in a list
+ Store all dictionaries in a list
 people = [person_1, person_2, person_3]
 
-# Loop through the list and print info about each person
+ Loop through the list and print info about each person
 for person in people:
     print("\nPerson Information:")
     for key, value in person.items():
